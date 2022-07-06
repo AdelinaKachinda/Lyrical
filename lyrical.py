fa7086a657909389c393a0e2c4d3e5bbfdc4587f
Adelina Kachinda <adelinakachinda@gmail.com>
2022-07-06 19:28:41 +0000

    lyrics using lyrical ovh

--- lyrical.py
+++ lyrical.py
@@ -1,34 +1,54 @@
 import os
 import json
+import requests
 import lyricsgenius
 
-genius = lyricsgenius.Genius("pN39MdQbjguuvRXwSxcZ9FuPYb5zh-DzG_QT5qTME7NWwhZHRiLzeHDUUP8dK1_7")
 
-artist_name = input("Enter artist name: ").capitalize()
-max_songs = int(input(f"How many songs of {artist_name} do you want? "))
+def get_lyrics(artist, title):
+  base_url = 'https://api.lyrics.ovh/v1/'
+  url = base_url + artist + "/" + title 
 
+  response = requests.get(url)
+  print(response)
 
-artist = genius.search_artist(artist_name, max_songs, sort="title")
-print(artist.songs)
-print("\n")
+  lyrics = response.json()
+  l_values = lyrics.values()
 
-song_name = input("Which song title amongst these do you want the lyrics to? ")
-song = genius.search_song(song_name, artist.name)
-# artist.save_lyrics()
+  for char in l_values:
+    print(char)
 
-print(type(song.lyrics))
 
 
+# # genius api
+# genius = lyricsgenius.Genius("pN39MdQbjguuvRXwSxcZ9FuPYb5zh-DzG_QT5qTME7NWwhZHRiLzeHDUUP8dK1_7")
 
+# artist_name = input("Enter artist name: ").capitalize()
+# max_songs = int(input(f"How many songs of {artist_name} do you want? "))
 
 
+# artist = genius.search_artist(artist_name, max_songs, sort="title")
+# print(artist.songs)
+# print("\n")
 
+# song_name = input("Which song title amongst these do you want the lyrics to? ")
+# song = genius.search_song(song_name, artist.name)
+# # artist.save_lyrics()
 
+# print(song.lyrics)
 
+def main():
+  user_response = list(input("Enter name of an artist \" + \"song: " + "(e.g Burna Boy + ye)").strip().split("+"))
+  artist_name = user_response[0].strip(" ")
+  song_name = user_response[1].strip(" ")
+  get_lyrics(artist_name, song_name)
+  
+  # print(user_response)
 
 
 
 
+if __name__ == "__main__":
+    main()
 
 
 
@@ -120,23 +140,16 @@ print(type(song.lyrics))
 
 
 
-# import requests
-# import pprint
-# import json
 
 
-# artist = input("Enter the name of the artist: ")
-# title = input("Enter the title of the song: ")
 
-# base_url = 'https://api.lyrics.ovh/v1/'
-# url = base_url + artist + "/" + title 
 
-# response = requests.get(url)
-# print(response)
 
-# lyrics = response.json()
-# l_values = lyrics.values()
 
-# for char in l_values:
-  # print(char)
+
+
+
+
+
+
 
